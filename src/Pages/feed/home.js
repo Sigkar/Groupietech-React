3331d50169a967d657c0fa21d441d4813d1c44ce
Duncan Pierce <duncan@duncanpierce.com>
2019-01-15 17:01:13 -0500

    Working on timestamps

--- src/Pages/feed/home.js
+++ src/Pages/feed/home.js
@@ -10,27 +10,18 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import "firebase/firestore";
 
-import {_getCollection} from '../../async/getCollection.js';
+import { _getCollection } from '../../async/getCollection.js';
 
 // Design
 import { PageContainer, ContentFeatureComponent } from '../../Components/cards.js';
 import { StaggerChildrenContent, LoadFade } from '../../Components/staticposes.js';
 import LoginImage from '../../Images/login-comp.jpeg';
 
-//const store = createStore;
-//     0:
-// content:
-// created_at: 1547427404.072
-// hearts: 0
-// posted_by: "Randombot"
-// text: "8y1tyr8y1tyr8y1tyr8y1tyr8y1tyr8y1tyr8y1tyr8y1tyr"
-// title: "8y1tyr"
-// updated_at: 1547427404.072
-// __proto__: Object
-// id: "35BDQ4oprEQsU4t0OL1a"
-const GenerateHomeContent = ({ current, items, total, rand }) => (
+const GenerateHomeContent = ({ current, items, total, rand, offset }) => (
     <div>
         {items.slice(current, total).map(item => (
+            console.log("Date: " + new Date(Math.ceil(item.content.created_at) * 1000)),
+
             <LoadFade key={item.id}>
                 <ContentFeatureComponent
                     imageLink={LoginImage}
@@ -58,6 +49,7 @@ export class Home extends Component {
             current: 0,
             total: 5,
             loadAnimations: false,
+            loadComplete: false,
             offset: new Date().getTimezoneOffset(),
         };
     }
@@ -67,68 +59,28 @@ export class Home extends Component {
         db.settings({
             timestampsInSnapshots: true
         });
-        
-        _getCollection("posts").then(
-            (res) => {
-                this.setState({
-                    isLoaded: true,
-                    items: res
-                });
-                setTimeout(this.toggleLoadAnimations, 500);
-            },
-            (error)=>{
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        );
 
+        this.res();
         window.scrollTo(0, 0);
-        // fetch("https://jsonplaceholder.typicode.com/posts")
-        //     .then(res => res.json())
-        //     .then(
-        //         (result) => {
-        //             this.setState({
-        //                 isLoaded: true,
-        //                 items: result
-        //             });
-        //             setTimeout(this.toggleLoadAnimations, 500);
-        //         },
-        //         (error) => {
-        //             this.setState({
-        //                 isLoaded: true,
-        //                 error
-        //             });
-        //         }
-        //     )
-    }
-    // makeRandomData(){
-    //     let _seconds = new Date() / 1000;
-    //     let db = firebase.firestore();
-    //     let r = Math.random().toString(36).substring(7);
-    //     let _hearts = 0;
-    //     let _posted_by = "Randombot";
-    //     let _text = r+r+r+r+r+r+r+r;
-    //     let _title = r;
-    //     let _created_at = _seconds;
-    //     let _updated_at = _seconds;
-    //     db.collection("posts").add({
-    //         text: _text,
-    //         title: _title,
-    //         created_at: _created_at,
-    //         updated_at: _updated_at,
-    //         posted_by: _posted_by,
-    //         hearts: _hearts
-    //     });
-    //     console.log("Created data.");
 
-    // }
+    }
+    res = () => _getCollection("posts").then((res) => {
+        this.setState({
+            isLoaded: true,
+            items: res
+        });
+        setTimeout(this.toggleLoadAnimations, 500);
+    },(error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        }
+    );
     toggleLoadAnimations = () => (this.setState({ loadAnimations: true }));
     getRandomArbitrary = (min, max) => { return Math.round(Math.random() * (max - min) + min) }
     render() {
         const { loadAnimations, error, isLoaded, items, current, total, offset } = this.state;
-        console.log(offset);
         if (error) {
             return <div>Sorry, Headlinerr has a bad sound guy!<br />Error: {error.message}</div>;
         } else if (!isLoaded) {
@@ -137,7 +89,7 @@ export class Home extends Component {
             return (
                 <PageContainer>
                     <StaggerChildrenContent pose={loadAnimations ? 'open' : 'closed'}>
-                        <GenerateHomeContent items={items} current={current} total={total} rand={this.getRandomArbitrary} />
+                        <GenerateHomeContent items={items} current={current} total={total} rand={this.getRandomArbitrary} offset={offset}/>
                         <p>Scroll support coming soon I guess.</p>
                     </StaggerChildrenContent>
                 </PageContainer>
